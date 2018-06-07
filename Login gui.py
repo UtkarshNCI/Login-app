402f365540f536b7df0ed009c0fa2a556a0da73c
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-07 13:49:37 +0530

    gui

--- Login gui.py
+++ Login gui.py
@@ -7,7 +7,7 @@ root.geometry("400x400")
 root.configure(background="snow")
 
 label1=tkinter.Label(text="Enter details and select platform",fg="black",font=("TkMenuFont",10),bg="misty rose")
-label1.grid(column=0)
+label1.grid(column=0,row=0,rowspan=2)
 
 rad1 = tkinter.Radiobutton(root,text='Facebook', value=1)
 rad2 = tkinter.Radiobutton(root,text='Twitter', value=2)
