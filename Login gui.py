c42f1133d36b8c8e25ca136de62a83263a7a6df0
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-07 13:48:48 +0530

    gui

--- Login gui.py
+++ Login gui.py
@@ -4,21 +4,31 @@ import os
 root=tkinter.Tk()
 root.title("Login App")
 root.geometry("400x400")
+root.configure(background="snow")
 
-label1=tkinter.Label(text="Enter details and select platform",fg="black",font=("Open Sans",10))
-label1.grid(column=0,row=0)
+label1=tkinter.Label(text="Enter details and select platform",fg="black",font=("TkMenuFont",10),bg="misty rose")
+label1.grid(column=0)
 
-label_usr=tkinter.Label(text="Username:",fg="Black",font=("Open Sans",10))
-label_usr.grid(column=0,row=1,sticky=tkinter.W)
+rad1 = tkinter.Radiobutton(root,text='Facebook', value=1)
+rad2 = tkinter.Radiobutton(root,text='Twitter', value=2)
+
+ 
+
+rad1.grid(column=0, row=2)
+rad2.grid(column=1, row=2)
+
+ 
+label_usr=tkinter.Label(text="Username",fg="Black",font=("TkMenuFont",10),bg="misty rose")
+label_usr.grid(column=0,row=3,sticky=tkinter.W)
 
 entry_usr=tkinter.Entry()
-entry_usr.grid(column=1,row=1,sticky=tkinter.W)
+entry_usr.grid(column=1,row=3)
 
-label_pwd=tkinter.Label(text="Password:",fg="Black",font=("Open Sans",10))
-label_pwd.grid(column=0,row=2,sticky=tkinter.W)
+label_pwd=tkinter.Label(text="Password",fg="Black",font=("TkMenuFont",10),bg="misty rose")
+label_pwd.grid(column=0,row=4,sticky=tkinter.W)
 
 entry_pwd=tkinter.Entry(show="*")
-entry_pwd.grid(column=1,row=2)
+entry_pwd.grid(column=1,row=4)
 
 
 
