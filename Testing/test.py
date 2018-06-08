561bdc9ab7fdd12d7177806a74d5901da7c42bcc
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-08 05:45:32 +0530

    test

--- Testing/test.py
+++ Testing/test.py
@@ -1,5 +1,6 @@
 import tkinter
-choice=tkinter.IntVar()
+
+
 root=tkinter.Tk()
 root.title("Login App")
 root.geometry("400x400")
@@ -8,9 +9,10 @@ root.configure(background="snow")
 label1=tkinter.Label(text="Enter details and select platform",fg="black",font=("TkMenuFont",10),bg="misty rose")
 label1.grid(column=0,row=0,rowspan=2)
 
-choice.set(1)
+choice=tkinter.IntVar()
 
 rad1 = tkinter.Radiobutton(root,text='Facebook', value=1,variable=choice)
 rad2 = tkinter.Radiobutton(root,text='Twitter', value=2,variable=choice)
-
+rad1.grid(column=0, row=2)
+rad2.grid(column=1, row=2)
 root.mainloop()
